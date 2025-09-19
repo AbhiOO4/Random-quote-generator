1f7104a516f7e6c440a8aa144138095934861c91
Abhinav Sreejith <abhinavsree243@gmail.com>
2025-09-19 16:34:57 +0530

    more css

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ const path = require('path')
 const quotes = require('./quotes.json')
 
 
+
+
+
 app.set('view engine','ejs');
 
 app.set('views', path.join(__dirname,'/views'))
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/random',(req,res) => {
     const num = Math.floor(Math.random()*30)
